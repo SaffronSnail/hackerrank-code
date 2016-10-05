cc8609231c61b9e31de9fb0c60b56b974f7fb07a
saffronsnail <saffronsnail@hushmail.com>
2016-10-05 16:06:10 -0700

    added old changes; continued bugfixing calculate
    I had written my conditional in the rotate_array function wrong, so
    it wasn't calling the version of the function for factors when it
    should have. That fixed the test case that I had found, but I still
    fail 3 tests on hackerrank.

--- circular_array_rotation/src/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "shared.h"
-
-void rotate_array(Array array, unsigned rots);
-
-#endif
-
--- circular_array_rotation/src/shared.h
+++ circular_array_rotation/src/shared.h
@@ -6,8 +6,10 @@ typedef struct
 	int *data;
 	unsigned size;
 } Array;
+Array make_array(int size);
 
 int circular_add(int op1, unsigned op2, int max);
+void rotate_array(Array array, unsigned rots);
 
 #endif
 
