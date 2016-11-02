2bf50f8919d25019115489d36d88dc4b147b5b23
saffronsnail <saffronsnail@hushmail.com>
2016-11-01 18:22:51 -0700

    Fixed bug with for loop
    My for loop was counting backwards, but it was counting backwards
    through the lower half of the array. It now counts upwards through the
    upper half of the array, which is what it should do.

--- insertion-sort-analysis/insert-sort-analysis.c
+++ insertion-sort-analysis/insert-sort-analysis.c
@@ -15,31 +15,30 @@ void read_args()
 		scanf("%d", array + i);
 }
 
-int count_all_shifts(int *array, int count)
+int count_all_shifts()
 {
 	int shifts = 0;
-
-	read_args();
+	memset(tallies, 0, MAX_VALUE);
 
 	for (int i = 0; i < count; ++i)
 	{
-		for (int j = array[i] - 1; j >= 0; --j)
+		for (int j = array[i] + 1; j < count; ++j)
 			shifts += tallies[j];
 		++tallies[array[i]];
 	}
 
-	memset(tallies, 0, count);
 	return shifts;
 }
 
 int main(void)
 {
-	memset(tallies, 0, MAX_VALUE);
-
 	int num_tests;
 	scanf("%d", &num_tests);
 	for (int i = 0; i < num_tests; ++i)
-		printf("%d\n", count_all_shifts(array, count));
+	{
+		read_args();
+		printf("%d\n", count_all_shifts());
+	}
 	return 0;
 }
 
