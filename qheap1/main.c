e8321cbe0efb8d0a85ba095139d2584e2a8253fd
saffronsnail <saffronsnail@hushmail.com>
2016-11-30 04:37:17 -0800

    updated implementation; designed interfaces
    The implementation now uses indexes as pointers instead of calculations
    to determine where parents and children are; this was to avoid having to
    copy values around the tree. I also created defined interfaces to the
    heap and heap nodes to make these sort of structural changes easier. I
    still need to finish the insert algorithm (replace_node) and implement
    the delete algorithm (which I'm pretty sure is pretty straight forward).

--- qheap1/main.c
+++ qheap1/main.c
@@ -2,102 +2,233 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // TYPE DEFINTIIONS
 typedef int Data;
 typedef unsigned Height;
-typedef unsigned Index;
+typedef unsigned HeapNodePtr;
 
-struct HeapData
+typedef struct HeapNode
 {
   Data data;
   Height height;
-  Index left;
-  Index right;
-};
+  HeapNodePtr left;
+  HeapNodePtr right;
+  HeapNodePtr parent;
+} HeapNode;
 
+// HEAP DEFNITION
 #define HEAP_LENGTH_DEF 100000
-static struct HeapData heap[HEAP_LENGTH_DEF];
+static HeapNode heap[HEAP_LENGTH_DEF];
+static HeapNodePtr head;
 
 // CONSTANTS
 static const int HEAP_LENGTH = HEAP_LENGTH_DEF;
 #undef HEAP_LENGTH_DEF
 
-static const Data EMPTY_NODE_VALUE = INT_MIN;
+static const Data  EMPTY_NODE_VALUE = INT_MIN;
+static const HeapNodePtr NULL_NODE = (unsigned)-1;
+
+// HEAP INTERFACE DECLARATIONS
+static inline void delete(Data data);
+static inline Data get_min();
+static inline void init();
+static inline void insert(Data data);
+
+// HEAPNODE INTERFACE DELCARATIONS
+static inline void init_node(HeapNodePtr this, Data data);
+static inline void delete_heap_node(HeapNodePtr this);
+static inline bool node_exists(HeapNodePtr this);
+
+static inline Data        get_data(HeapNodePtr this);
+static inline Height      get_height(HeapNodePtr this);
+static inline HeapNodePtr get_left(HeapNodePtr this);
+static inline HeapNodePtr get_parent(HeapNodePtr this);
+static inline HeapNodePtr get_right(HeapNodePtr this);
+
+static inline void set_data(HeapNodePtr this, Data data);
+static inline void set_height(HeapNodePtr this, Height height);
+static inline void set_left(HeapNodePtr this, HeapNodePtr new_left);
+static inline void set_parent(HeapNodePtr this, HeapNodePtr new_parent);
+static inline void set_right(HeapNodePtr this, HeapNodePtr new_right);
+
+static inline void increment_height(HeapNodePtr this);
+static inline void decrement_height(HeapNodePtr this);
 
 // HELPER FUNCTION DECLARATIONS
 static inline bool is_odd(int arg);
 
-static inline Index left_index(Index index);
-static inline Index parent_index(Index index);
-static inline Index right_index(Index index);
-
-static inline bool node_exists(Index index);
-
-static void insert_aux(Data data, Index index);
-static inline void insert(Data data);
+static void insert_aux(Data data, HeapNodePtr this, HeapNodePtr parent);
+static void replace_node(Data data, HeapNodePtr this);
 
 // MAIN
-int main(void)
-{
-  for (Index i = 0; heap[i].data < HEAP_LENGTH; ++i)
-    heap[i].data = EMPTY_NODE_VALUE;
+int main(void) {
+  init();
+  unsigned num_queries;
+  scanf("%d", &num_queries);
+  for (unsigned curr_query = 0; curr_query < num_queries; ++curr_query)
+  {
+    unsigned query;
+    scanf("%d", &query);
 
+    if (query == 3)
+      printf("%d\n", get_min());
+    else
+    {
+      Data arg;
+      scanf("%d", &arg);
+      
+      if (query == 1)
+        insert(arg);
+      else
+        delete(arg);
+    }
+  }
   return 0;
 }
 
-// HELPER FUNCTION DEFINITIONS
-static inline bool is_odd(int arg)
+// HEAP INTERFACE DEFINITIONS
+void delete(Data data)
 {
-  return arg & 1;
+#error TODO: implement delete funciton
+}
+
+Data get_min()
+{
+  return heap[head].data;
+}
+
+void init()
+{
+  head = 0;
+  for (HeapNodePtr i = 0; heap[i].data < HEAP_LENGTH; ++i)
+    heap[i].data = EMPTY_NODE_VALUE;
 }
 
 void insert(Data data)
 {
-  insert_aux(data, 0);
+  insert_aux(data, head, NULL_NODE);
 }
 
-void insert_aux(Data data, Index index)
+// HEAPNODE INTERFACE DEFINITIONS
+void decrement_height(HeapNodePtr this)
 {
-  if(!node_exists(index))
-  {
-    heap[index].data = data;
-    heap[index].height = 0;
-  }
-  else if (heap[index].data < data)
-  {
-    
-  }
+  heap[this].height -= 1;
+}
+
+void delete_heap_node(HeapNodePtr this)
+{
+  set_data(this, EMPTY_NODE_VALUE);
+}
+
+void increment_height(HeapNodePtr this)
+{
+  heap[this].height += 1;
+}
+
+void init_node(HeapNodePtr this, Data data)
+{
+  set_data(this, data);
+  set_height(this, 0);
+  set_left(this, NULL_NODE);
+  set_right(this, NULL_NODE);
+  set_parent(this, NULL_NODE);
+}
+
+Data get_data(HeapNodePtr this)
+{
+  return heap[this].data;
+}
+
+Height get_height(HeapNodePtr this)
+{
+  return heap[this].height;
+}
+
+HeapNodePtr get_left(HeapNodePtr this)
+{
+  return heap[this].left;
+}
+
+HeapNodePtr get_parent(HeapNodePtr node)
+{
+  return heap[node].parent;
+}
+
+HeapNodePtr get_right(HeapNodePtr node)
+{
+  return heap[node].right;
+}
+
+bool node_exists(HeapNodePtr node)
+{
+  return heap[node].data == EMPTY_NODE_VALUE;
+}
+
+void set_data(HeapNodePtr this, Data data)
+{
+  heap[this].data = data;
+}
+
+void set_height(HeapNodePtr this, Height height)
+{
+  heap[this].height = height;
+}
+
+void set_left(HeapNodePtr this, HeapNodePtr left)
+{
+  heap[this].left = left;
+}
+
+void set_parent(HeapNodePtr this, HeapNodePtr parent)
+{
+  heap[this].parent = parent;
+}
+
+void set_right(HeapNodePtr this, HeapNodePtr right)
+{
+  heap[this].right = right;
+}
+
+// HELPER FUNCTION DEFINITIONS
+void insert_aux(Data data, HeapNodePtr node, HeapNodePtr parent)
+{
+  if(!node_exists(node))
+    init_node(node, data);
+  else if (heap[node].data > data)
+    replace_node(data, node);
   else
   {
-    heap[index].height += 1;
-    Index left = left_index(index);
-    Index right = right_index(index);
+    increment_height(node);
 
-    if (heap[left].height < heap[right].height)
-      insert_aux(data, left);
+    HeapNodePtr left_node = left(node);
+    HeapNodePtr right_node = right(node);
+
+    if (height_of(left_node) < height_of(right_node))
+      insert_aux(data, left_node, node);
     else
-      insert_aux(data, right);
+      insert_aux(data, right_node, node);
   }
 }
 
-Index left_index(Index index)
+bool is_odd(int arg)
 {
-  return right_index(index) - 1;
+  return arg & 1;
 }
 
-static inline bool node_exists(Index index)
+void replace_node(Data data, HeapNodePtr target)
 {
-  return heap[index].data == EMPTY_NODE_VALUE;
-}
+  HeapNodePtr parent = heap[target].parent;
 
-Index perent_index(Index index)
-{
-  return (is_odd(index) ? (index + 1) / 2 : index / 2) - 1;
-}
+  HeapNodePtr new_node = new_heap_node();
+  init_heap_node(new_node, data, parent);
 
-Index right_index(Index index)
-{
-  return (index + 1) * 2;
+  if (heap[parent].left == new_node)
+    heap[parent].left = new_node;
+  else
+    heap[parent].right = new_node;
+
+# error TODO: finish implementing replace_node
 }
 
