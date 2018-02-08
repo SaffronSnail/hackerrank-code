00ec127785e381454fec1412dc415a174cf50a57
Bryan Ferris <saffsnail@gmail.com>
2018-02-08 14:56:34 -0800

    Refactored comonents-in-graph to be more performant and less complicated; documentation to follow

--- Data_Structures/Disjoint_Set/components-in-graph/submission.cpp
+++ Data_Structures/Disjoint_Set/components-in-graph/submission.cpp
@@ -1,113 +1,79 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
-using std::cin;
-using std::cout;
 using std::endl;
 
 typedef int Vertex;
+typedef std::unordered_map<Vertex, std::unordered_set<Vertex> > AdjacencyList;
 typedef std::unordered_set<Vertex> Graph;
+typedef std::vector<Graph> GraphSet;
 
-typedef std::pair<Vertex, Vertex> Edge;
-std::ostream &operator<<(std::ostream &out, const Edge &edge)
-{
-  out << "(" << edge.first << ", " << edge.second << ")";
-  return out;
+void extract_graph_aux(AdjacencyList &list, const AdjacencyList::iterator &current, Graph &result) {
+  for (auto neighbor : current->second)
+    if (result.insert(neighbor).second)
+      extract_graph_aux(list, list.find(neighbor), result);
 }
 
-static std::unordered_map<Vertex, Graph *> graphs;
+Graph extract_graph(AdjacencyList &list) {
+  Graph ret;
 
-void add_edge(Vertex v1, Vertex v2);
-void add_to_graph(Graph *graph, Vertex v);
-void create_new_graph(Vertex v1, Vertex v2);
-void merge_graphs(Graph *source, Graph *destination);
+  auto origin = list.begin();
+  ret.insert(origin->first);
+  extract_graph_aux(list, origin, ret);
 
-struct Result
-{
-  size_t min;
-  size_t max;
-};
-
-Result find_result();
-
-int main(void)
-{
-  size_t n;
-  Vertex left, right;
-
-  cin >> n;
-  
-  for (size_t i = 0; i < n; ++i)
-  {
-    cin >> left >> right;
-    add_edge(left, right);
-  }
-
-  Result res = find_result();
-  cout << res.min << " " << res.max << endl;
-}
-
-void add_edge(Vertex v1, Vertex v2)
-{
-  bool v1_exists = graphs.count(v1) == 1;
-  bool v2_exists = graphs.count(v2) == 1;
-
-  if (v1_exists && v2_exists)
-    merge_graphs(graphs.at(v1), graphs.at(v2));
-  else if (v1_exists)
-    add_to_graph(graphs.at(v1), v2);
-  else if (v2_exists)
-    add_to_graph(graphs.at(v2), v1);
-  else
-    create_new_graph(v1, v2);
-}
-
-void add_to_graph(Graph *graph, Vertex v)
-{
-  graph->insert(v);
-  graphs[v] = graph;
-}
-
-void create_new_graph(Vertex v1, Vertex v2)
-{
-  Graph *graph = new Graph();
-
-  add_to_graph(graph, v1);
-  add_to_graph(graph, v2);
-}
-
-Result find_result()
-{
-  Result ret;
-  ret.min = SIZE_MAX;
-  ret.max = 0;
-
-  std::unordered_set<Graph *> checked;
-  for (auto kvp : graphs)
-  {
-    if (checked.count(kvp.second) == 0)
-    {
-      checked.insert(kvp.second);
-      size_t length = kvp.second->size();
-      if (length < ret.min && length > 1)
-        ret.min = length;
-      if (length > ret.max)
-        ret.max = length;
-    }
-  }
-
+  for (Vertex v : ret)
+    list.erase(v);
   return ret;
 }
 
-void merge_graphs(Graph *source, Graph *destination)
-{
-  if (source == destination)
-    return;
+std::istream &operator>>(std::istream &in, GraphSet &gs) {
+  size_t n;
+  in >> n;
 
-  for (Vertex v : *source)
-    add_to_graph(destination, v);
+  AdjacencyList edges;
+  edges.reserve(n * 2);
 
-  delete source;
+  for (size_t i = 0; i < n; ++i)
+  {
+    Vertex v1, v2;
+    in >> v1 >> v2;
+    edges[v1].insert(v2);
+    edges[v2].insert(v1);
+  }
+
+  while (!edges.empty())
+    gs.push_back(extract_graph(edges));
+
+  return in;
 }
 
+std::ostream &operator<<(std::ostream &os, Graph &set) {
+  for (Vertex v : set)
+    os << v << " ";
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, GraphSet &set) {
+  for (Graph g : set)
+    os << g << endl;
+  return os;
+}
+
+int main(void)
+{
+  GraphSet data;
+  std::cin >> data;
+
+  size_t min = SIZE_MAX, max = 0;
+
+  for (const auto &graph : data)
+  {
+    size_t length = graph.size();
+    min = std::min(length, min);
+    max = std::max(length, max);
+  }
+
+  std::cout << min << " " << max << endl;
+}
