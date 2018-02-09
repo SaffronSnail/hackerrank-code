75b02dd24d568caae7ab3b28f41074533266d5f4
Bryan Ferris <saffsnail@gmail.com>
2018-02-08 16:13:31 -0800

    Reorganized & commented components-in-graph

--- Data_Structures/Disjoint_Set/components-in-graph/submission.cpp
+++ Data_Structures/Disjoint_Set/components-in-graph/submission.cpp
@@ -1,3 +1,7 @@
+// This implementation reads all of the supplied edges into an adjacency list,
+// extracts each graph from the list, then checks all the graphs to find the
+// solutian
+
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
@@ -10,56 +14,12 @@ typedef std::unordered_map<Vertex, std::unordered_set<Vertex> > AdjacencyList;
 typedef std::unordered_set<Vertex> Graph;
 typedef std::vector<Graph> GraphSet;
 
-void extract_graph_aux(AdjacencyList &list, const AdjacencyList::iterator &current, Graph &result) {
-  for (auto neighbor : current->second)
-    if (result.insert(neighbor).second)
-      extract_graph_aux(list, list.find(neighbor), result);
-}
+// Extracts all of the vertices from one subgraph in the AdjacencyList and
+// stores them in the graph
+Graph extract_graph(AdjacencyList &list);
 
-Graph extract_graph(AdjacencyList &list) {
-  Graph ret;
-
-  auto origin = list.begin();
-  ret.insert(origin->first);
-  extract_graph_aux(list, origin, ret);
-
-  for (Vertex v : ret)
-    list.erase(v);
-  return ret;
-}
-
-std::istream &operator>>(std::istream &in, GraphSet &gs) {
-  size_t n;
-  in >> n;
-
-  AdjacencyList edges;
-  edges.reserve(n * 2);
-
-  for (size_t i = 0; i < n; ++i)
-  {
-    Vertex v1, v2;
-    in >> v1 >> v2;
-    edges[v1].insert(v2);
-    edges[v2].insert(v1);
-  }
-
-  while (!edges.empty())
-    gs.push_back(extract_graph(edges));
-
-  return in;
-}
-
-std::ostream &operator<<(std::ostream &os, Graph &set) {
-  for (Vertex v : set)
-    os << v << " ";
-  return os;
-}
-
-std::ostream &operator<<(std::ostream &os, GraphSet &set) {
-  for (Graph g : set)
-    os << g << endl;
-  return os;
-}
+// Reads input as specified on hackerrank
+std::istream &operator>>(std::istream &in, GraphSet &gs);
 
 int main(void)
 {
@@ -76,4 +36,47 @@ int main(void)
   }
 
   std::cout << min << " " << max << endl;
+}
+
+// responsible for adding all neighbors and indirect neighbors
+void extract_graph_aux(AdjacencyList &list, const AdjacencyList::iterator &current, Graph &result) {
+  for (auto neighbor : current->second)
+    if (result.insert(neighbor).second)
+      extract_graph_aux(list, list.find(neighbor), result);
+}
+
+// responsible for adding the first point of the graph and removing encountered
+// nodes from the adjacency list
+Graph extract_graph(AdjacencyList &list) {
+  Graph ret;
+
+  auto origin = list.begin();
+  ret.insert(origin->first);
+  extract_graph_aux(list, origin, ret);
+
+  for (Vertex v : ret)
+    list.erase(v);
+  return ret;
+}
+
+// Reads based on the format that hackerrank happens to use
+std::istream &operator>>(std::istream &in, GraphSet &gs) {
+  size_t n;
+  in >> n;
+
+  AdjacencyList edges;
+  edges.reserve(n * 2);
+
+  for (size_t i = 0; i < n; ++i)
+    {
+      Vertex v1, v2;
+      in >> v1 >> v2;
+      edges[v1].insert(v2);
+      edges[v2].insert(v1);
+    }
+
+  while (!edges.empty())
+    gs.push_back(extract_graph(edges));
+
+  return in;
 }
